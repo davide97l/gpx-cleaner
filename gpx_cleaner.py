cddc9e66438f5107a67c2584d4d69a027145b80d
Andreas Sommer <andreas.sommer87@googlemail.com>
2023-09-25 09:24:35 +0200

    Make `gpx_cleaner.py` usable as command line tool

--- gpx_cleaner.py
+++ gpx_cleaner.py
@@ -1,9 +1,12 @@
-import gpxpy.gpx
+#!/usr/bin/env python3
+import argparse
 import datetime
+
+import gpxpy.gpx
 from geopy.distance import distance
 
 
-def run(activity_gpx):
+def run(activity_gpx, min_pause_seconds=1):
 
     gpx = activity_gpx
     removed = datetime.timedelta()
@@ -23,11 +26,13 @@ def run(activity_gpx):
                 if last is not None:
                     last_point = segment.points[i-1]
                     d = distance((point.latitude, point.longitude), (last_point.latitude, last_point.longitude)).m
-                    if time - last > datetime.timedelta(seconds=1):
+                    if time - last >= datetime.timedelta(seconds=min_pause_seconds):
                         print('Pause {}: {}s | {:.3f}m'.format(stops+1, time - last, d))
                         ret_data['Pause {}'.format(stops+1)] = [time - last, d]
                         removed += time - last
                         stops += 1
+                    elif d > 10:
+                        raise AssertionError('Long distance between points. Handle?')
                     else:
                         tot_dist += d
                 if removed > datetime.timedelta():
@@ -47,11 +52,24 @@ def run(activity_gpx):
 
 
 if __name__ == '__main__':
-    activity_gpx = 'gpx/activity_177944060.gpx'
-    gpx_file = open(activity_gpx, 'r')
+    parser = argparse.ArgumentParser(prog='gpx_cleaner')
+    parser.add_argument(
+        '--min-pause-seconds',
+        default=1,
+        type=int,
+        help='If the time distance between two time points is larger than this, the track will be split.')
+    parser.add_argument(
+        'input_file_path',
+        help='Input GPX file')
+    parser.add_argument(
+        'output_file_path',
+        help='Output GPX file')
+    args = parser.parse_args()
+
+    activity_gpx = '/Users/asommer/Downloads/Zepp20230921114213.gpx'
+    gpx_file = open(args.input_file_path, 'r')
     gpx = gpxpy.parse(gpx_file)
-    activity_name = activity_gpx.split('.')[0] + '_clean.gpx'
-    gpx_xml, data = run(gpx)
-    with open(activity_name, 'w') as f:
+    gpx_xml, data = run(gpx, min_pause_seconds=args.min_pause_seconds)
+    with open(args.output_file_path, 'w') as f:
         f.write(gpx_xml)
-    print('Created {}'.format(activity_name))
+    print('Created {}'.format(args.output_file_path))
